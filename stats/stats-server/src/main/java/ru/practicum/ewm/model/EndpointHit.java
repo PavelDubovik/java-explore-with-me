adad062dd0f235a11f2c7abb71ad88883e8b7404
Pavel Dubovik <pavel.djub@gmail.com>
2023-02-01 20:20:19 +0200

    feature: docker

--- stats/stats-server/src/main/java/ru/practicum/ewm/model/EndpointHit.java
+++ stats/stats-server/src/main/java/ru/practicum/ewm/model/EndpointHit.java
@@ -14,8 +14,12 @@ public class EndpointHit {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(name = "app")
     private String app;
+    @Column(name = "uri")
     private String uri;
+    @Column(name = "ip")
     private String ip;
+    @Column(name = "time_stamp")
     private LocalDateTime timestamp;
 }
